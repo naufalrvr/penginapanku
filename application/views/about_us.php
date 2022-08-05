acafe594f1b899438216115c1d3f63aa99b5eaed
naufalrvr <elekkikuk@gmail.com>
2022-08-05 22:13:45 +0700

    first commit

---
+++ application/views/about_us.php
@@ -0,0 +1,8 @@
+<div class="container">
+  <div class="row">
+    <div class="col-md-12" style="margin-top: 60px;">
+      <center><h4 class="text-purple">penginapanku</h4></center>
+      <p class="text-center">penginapanku adaah sistem layanan booking hotel.</p>
+    </div>
+  </div>
+</div>
---
+++ application/views/home.php
@@ -0,0 +1,33 @@
+
+<img src="images/hd.jpg" class="img-fluid" alt="...">
+
+<div class="p-2">
+	
+	<a class="btn btn-primary btn-lg container-fluid" href="<?=base_url('order/apartements')?>" role="button">Pesan Sekarang</a>
+</div>
+
+
+<!-- img -->
+<img src="images/poster.png" class="img-fluid pt-5 rounded " alt="...">
+<!-- end img -->
+
+<!-- card -->
+<div class="pt-5">
+	<div class="card mb-3 container-fluid" style="max-width: 90%;">
+		<div class="row g-0">
+			<div class="col-md-4">
+				<img src="images/card.jpg" class="img-fluid rounded-start" alt="...">
+			</div>
+			<div class="col-md-8">
+				<div class="card-body">
+					<h1 class="card-title pt-3">Dapatkan</h1>
+					<h3 class="pt-3">tempat menginap terbaik anda di sini</h3>
+					<div class="pt-5">
+						<a class="btn btn-primary btn-lg " href="<?=base_url('order/apartements')?>" role="button">Pesan Sekarang</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+	<!-- end card -->
